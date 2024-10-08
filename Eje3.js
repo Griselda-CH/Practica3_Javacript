c1c6b1f203532e05d9efece921f41333068d01c7
Griselda-CH <gris01cordova@gmail.com>
2024-10-07 20:43:23 -0600

    Actualizar

--- Eje3.js
+++ Eje3.js
@@ -1,6 +1,6 @@
 /*3.- Determina si una palabra o frase es un palíndromo
 Desarrolla una función que reciba una cadena y determine 
 si es un palíndromo. Debe ignorar los espacios y mayúsculas.
- Puedes usar un bucle para comparar los caracteres desde el principio y el final,
-  acercándote hacia el centro de la cadena.*/
+Puedes usar un bucle para comparar los caracteres desde el principio y el final,
+acercándote hacia el centro de la cadena.*/
   
--- Eje4.js
+++ Eje4.js
@@ -1,3 +1,22 @@
 /*4.- Devuelve los primeros n números de la sucesión de Fibonacci
 Crea una función que genere los primeros n números de la sucesión de Fibonacci.
  Debes evitar la recursividad para mejorar el rendimiento cuando n es grande*/
+
+ function fibonacci(n) {
+    let fibSeries = [];
+    
+    for (let i = 0; i < n; i++) {
+        if (i === 0) {
+            fibSeries.push(0); 
+        } else if (i === 1) {
+            fibSeries.push(1); 
+        } else {
+            let CFib = fibSeries[i - 1] + fibSeries[i - 2];
+            fibSeries.push(CFib); 
+        }
+    }
+    
+    return fibSeries;
+}
+
+console.log(fibonacci(1));
